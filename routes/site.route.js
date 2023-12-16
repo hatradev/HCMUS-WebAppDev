ff63987e2ff68e147bfa738f40c4c0cf5ac8428a
dinhquytrieu <dqtrieu21@clc.fitus.edu.vn>
2023-12-16 17:16:20 +0700

    update header, nav, cart page and payment page

--- routes/site.route.js
+++ routes/site.route.js
@@ -4,5 +4,6 @@ const siteController = require("../controllers/site.controller");
 const router = express.Router();
 router.get("/", siteController.getHome);
 router.get("/my-cart", siteController.getCart);
+router.get("/payment", siteController.getPayment);
 
 module.exports = router;
