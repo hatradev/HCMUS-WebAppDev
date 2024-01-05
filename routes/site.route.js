d9beb598fd9aa99f83741e9e67a2efbaf319547f
21127453 <hatra21@clc.fitus.edu.vn>
2024-01-06 01:32:46 +0700

    update

--- routes/site.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const siteController = require("../controllers/site.controller");
-
-const router = express.Router();
-router.get("/", siteController.getHome);
-router.get("/my-cart", siteController.getCart);
-
-module.exports = router;
