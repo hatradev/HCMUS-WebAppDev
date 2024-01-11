6bd0fbd4be2e3e6f6bb6db80631f573238c529e4
21127453 <hatra21@clc.fitus.edu.vn>
2024-01-11 11:03:57 +0700

    Update profile.controller.js

--- main/controllers/profile.controller.js
+++ main/controllers/profile.controller.js
@@ -1,20 +1,28 @@
-const User = require('../models/account.model');
+const User = require("../models/account.model");
 
 class profileController {
   // [GET] /
   getProfile = async (req, res, next) => {
     try {
       const userId = req.session.passport.user;
-      const user = await User.findOne({_id: userId});
+      const user = await User.findOne({ _id: userId });
       console.log(user);
       const lastname = user.lastname,
-      firstname = user.firstname,
-      phone = user.phone,
-      email = user.email,
-      provinces = user.provinces,
-      district = user.district,
-      detailAddress = user.detailAddress;
-      res.render("profile", {lastname, firstname, phone, email, provinces, district, detailAddress});
+        firstname = user.firstname,
+        phone = user.phone,
+        email = user.email,
+        provinces = user.provinces,
+        district = user.district,
+        detailAddress = user.detailAddress;
+      res.render("profile", {
+        lastname,
+        firstname,
+        phone,
+        email,
+        provinces,
+        district,
+        detailAddress,
+      });
     } catch (err) {
       next(err);
     }
@@ -25,13 +33,22 @@ class profileController {
     } catch (err) {
       next(err);
     }
-  }
+  };
 
   //[POST]
   updateProfile = async (req, res, next) => {
     try {
-      const {inputFName, inputLName, inputEmail, calc_shipping_provinces, calc_shipping_district, inputAddress, inputPhoneNumber } = req.body;
-      await User.updateOne({email: inputEmail}, 
+      const {
+        inputFName,
+        inputLName,
+        inputEmail,
+        calc_shipping_provinces,
+        calc_shipping_district,
+        inputAddress,
+        inputPhoneNumber,
+      } = req.body;
+      await User.updateOne(
+        { email: inputEmail },
         {
           firstname: inputFName,
           lastname: inputLName,
@@ -39,14 +56,15 @@ class profileController {
           provinces: calc_shipping_provinces,
           district: calc_shipping_district,
           detailAddress: inputAddress,
-          phone: inputPhoneNumber
-      });
-      res.redirect('/profile');
-      console.log('update information successfully')
+          phone: inputPhoneNumber,
+        }
+      );
+      res.redirect("/profile");
+      console.log("update information successfully");
     } catch (err) {
       next(err);
     }
-  }
+  };
 }
 
 module.exports = new profileController();
