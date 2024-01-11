f6a0b16f8ef00a47773b0fd3ae76ccd99c1afb72
21127453 <hatra21@clc.fitus.edu.vn>
2024-01-11 09:19:46 +0700

    sth about fe

--- main/models/order.model.js
+++ main/models/order.model.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  idaccount: { type: mongoose.Schema.Types.ObjectId, ref: 'ms.account' },
+  idaccount: { type: mongoose.Schema.Types.ObjectId, ref: 'account' },
   name: String,
   phone: String,
   email: String,
   address: String,
   message: String,
   detail: [{
-    idProduct: { type: mongoose.Schema.Types.ObjectId, ref: 'ms.product' },
+    idProduct: { type: mongoose.Schema.Types.ObjectId, ref: 'product' },
     quantity: Number
   }],
   status: { type: String, enum: ['canceled', 'pending', 'successful'], required: true },
