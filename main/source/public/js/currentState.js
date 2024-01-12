df5294309a862bb7acb81d9fcbe081d5b82fe84f
phuchaitt3 <phuchaitt3@gmail.com>
2024-01-12 17:48:22 +0700

    Before client-side all products

---
+++ main/source/public/js/currentState.js
@@ -0,0 +1,6 @@
+let currentState = {
+    mode: 'normal', // Can be 'filter', 'search', or 'normal'
+    page: 1,
+    filterParams: {}, // Parameters used for filtering
+    searchQuery: '' // Search query string
+};
