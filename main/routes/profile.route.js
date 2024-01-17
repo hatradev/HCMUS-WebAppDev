f023e39a347e841abc9f6997bc785f0736a209ba
lephuongchi03kh@gmail.com <lephuongchi03kh@gmail.com>
2024-01-17 10:55:37 +0700

    change avatar (upload file)

--- main/routes/profile.route.js
+++ main/routes/profile.route.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const profileController = require("../controllers/profile.controller");
 const userController = require('../controllers/user.controller');
+const multer = require("multer");
+const upload = multer({ dest: 'main/source/public/img/avatar' });
 
 const router = express.Router();
 
@@ -9,7 +11,7 @@ router.use(userController.checkRole('user'));
 router.get("/", profileController.getProfile);
 router.get("/changepassword", profileController.getChangePasswordP);
 
-router.post("/update", profileController.updateProfile);
+router.post("/update", upload.single('img'), profileController.updateProfile);
 router.post("/changepassword", profileController.changePassword);
 
 
