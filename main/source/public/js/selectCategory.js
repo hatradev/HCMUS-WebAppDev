75df6fd0956d9c3b1aec452b9ce8f1343e6a6c1f
21127453 <hatra21@clc.fitus.edu.vn>
2024-01-17 13:30:49 +0700

    merge

--- main/source/public/js/selectCategory.js
+++ main/source/public/js/selectCategory.js
@@ -21,34 +21,39 @@
 // }
 
 function selectCategory(categoryId, event) {
-    if (event) {
-        event.preventDefault(); // Prevent the default anchor behavior
-        event.currentTarget.classList.add('selected-category');
+  if (event) {
+    event.preventDefault(); // Prevent the default anchor behavior
+  }
+
+  document.getElementById("selectedCategory").value = categoryId;
+
+  // Remove the 'selected-category' class from all category links
+  document.querySelectorAll(".category-tree a").forEach((cat) => {
+    cat.classList.remove("selected-category");
+  });
+
+  // If event is not provided, find the corresponding element manually
+  if (!event) {
+    const selectedElement = document.querySelector(
+      `[data-category-id="${categoryId}"]`
+    );
+    if (selectedElement) {
+      selectedElement.classList.add("selected-category");
     }
-
-    document.getElementById('selectedCategory').value = categoryId;
-
-    // Remove the 'selected-category' class from all category links
-    document.querySelectorAll('.category-tree a').forEach(cat => {
-        cat.classList.remove('selected-category');
-    });
-
-    // If event is not provided, find the corresponding element manually
-    if (!event) {
-        const selectedElement = document.querySelector(`[data-category-id="${categoryId}"]`);
-        if (selectedElement) {
-            selectedElement.classList.add('selected-category');
-        }
-    }
+  } else {
+    event.currentTarget.classList.add("selected-category");
+  }
 }
 
 // Call selectCategory for the selected category on page load
-document.addEventListener('DOMContentLoaded', () => {
-    const categoryContainer = document.getElementById('categoryContainer');
-    const selectedCategoryId = categoryContainer.getAttribute('data-selected-category');
-    // console.log(selectedCategoryId);
-    
-    if (selectedCategoryId) {
-        selectCategory(selectedCategoryId);
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  const categoryContainer = document.getElementById("categoryContainer");
+  const selectedCategoryId = categoryContainer.getAttribute(
+    "data-selected-category"
+  );
+  // console.log(selectedCategoryId);
+
+  if (selectedCategoryId) {
+    selectCategory(selectedCategoryId);
+  }
 });
