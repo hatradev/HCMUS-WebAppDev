b08d96424d968146662b88e10c82239f86ddd05f
Dinh Quy Trieu <101011727+dinhquytrieu@users.noreply.github.com>
2024-09-13 13:58:24 +0700

    Update server.js

--- main/source/server.js
+++ main/source/server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 database.connect();
 const port = process.env.MAIN_PORT || 3000;
-const host = process.env.HOST || "0.0.0.0";
+const host = process.env.HOST || "127.0.0.1";
 app.listen(port, host, () => {
   console.log(`Main app is running on port ${port} ...`);
 });
